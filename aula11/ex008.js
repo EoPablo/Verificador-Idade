0adfba011566a799f313b4e2c4dfa0c131910e2f
Pablo <103059793+EoPablo@users.noreply.github.com>
2022-11-27 22:51:53 -0300

    Delete aula11 directory

--- aula11/ex008.js
+++
@@ -1,8 +0,0 @@
-var vel = 12
-console.log(`A velocidade do seu carro é de ${vel} km/h`)
-if (vel > 60) {
-    console.log(`Você ultrapassou a velocidade permitida. MULTADO !`)
-}
-
-
-console.log('Diriga sem com cinto de segurança')
